a0ee61bc8bf12024adf0b5c6f280d53d126b5a0d
Derek Harne <derek.harne@gmail.com>
2023-04-18 21:21:35 -0500

    Initial Commit

---
+++ ChatGPT-FlappyBird/ChatGPT_FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_FlappyBirdApp.swift
+//  ChatGPT-FlappyBird
+//
+//  Created by Derek Harne on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
